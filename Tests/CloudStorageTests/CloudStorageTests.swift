946bf81df6e73098fcc7d6ec1db82144928d46be
Varun Santhanam <talkto@vsanthanam.com>
2023-07-23 17:43:27 -0700

    Initial Commit

---
+++ Tests/CloudStorageTests/CloudStorageTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CloudStorage
+
+final class CloudStorageTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CloudStorage().text, "Hello, World!")
+    }
+}
